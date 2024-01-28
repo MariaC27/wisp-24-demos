6b5e0830765afcc960f13706b733bd45eccfce45
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2024-01-28 16:12:23 -0500

    add edit callback

--- react-demo/notes-bored/src/Note2.js
+++ react-demo/notes-bored/src/Note2.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
 
 const Note2 = (props) => {
+    // state variable boolean "editing"
+    const [editing, setEditing] = useState(false);
 
+    // newNoteName and newNoteText state and functions same as those on the initial create! 
+    const [newNoteName, setNewNoteName] = useState('');
+    const [newNoteText, setNewNoteText] = useState('');
 
-    // using callback function from props!
-    const deleteNote = () => {
-        props.delete(props.id)
+    // these functions help with what is called controlled inputs!
+    const newNoteNameFunction = (event) => {
+        setNewNoteName(event.target.value);
     }
 
-    return (
-        <div className="note">
-        <p>{props.name}</p>
-        <p>{props.text}</p>
-        <button onClick={deleteNote}>delete</button>
-        </div>
-    )
+    const newNoteTextFunction = (event) => {
+        setNewNoteText(event.target.value);
+    }
+
+    // in this case we DO want to make a separate function instead of just using an anonymous one, 
+    // because we want to do multiple things in this function
+    const saveNote = (event) => {
+        // using edit callback function from props
+        props.edit(props.id, newNoteName, newNoteText)
+        setEditing(false)
+    }
+
+
+    // conditional rendering
+
+    // if not editing, just display note
+    if (editing === false) {
+        return (
+            <div className="note">
+                <p>{props.name}</p>
+                <p>{props.text}</p>
+                <button onClick={() => (props.delete(props.id))}>delete</button>
+                <button onClick={() => setEditing(true)}>edit</button>
+            </div>
+        )
+    }
+
+    else {
+        return (
+            <div className="note">
+             <p>enter title:</p>
+                <input type="text" value={newNoteName} onChange={newNoteNameFunction} />
+
+            <p>enter text:</p>
+                <input type="text" value={newNoteText} onChange={newNoteTextFunction} />
+            <button onClick={saveNote}>save</button>
+            </div>
+        )
+    }
+    
 }
 
 export default Note2;
--- react-demo/notes-bored/src/NoteBoard2.js
+++ react-demo/notes-bored/src/NoteBoard2.js
@@ -29,6 +29,26 @@ const NoteBoard2 = (props) => {
         setNotes(notes.filter((i) => i.id !== id));
     }
 
+    //callback function to change name and text of specific note
+    const editNote = (id, newName, newText) => {
+
+        // JS concept of IMMUTABILITY: need to create a copy of the array and set that as the 
+        // new note state, can't "mutate" the old state array in place!
+        const newNotes = notes.map((note, i) => {
+            if (i === id) {
+              // if matches the ID in question, modify the note!
+              return {id: id, name: newName, text: newText};
+            } else {
+              // The rest haven't changed
+              return note;
+            }
+          });
+
+          // finally, set the state to the new array with modified note!
+          setNotes(newNotes);
+    
+    }
+
     
     const allNotes = notes.map((note) => {
         return (
@@ -38,6 +58,7 @@ const NoteBoard2 = (props) => {
                 id={note.id}
                 key={note.id}
                 delete={deleteNote} // passing callback function!
+                edit={editNote}
             />
             );
         }
